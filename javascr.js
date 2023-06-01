9090a8650c792e00d94562234ee2d1843cd5c229
shlok-shah <55475572+shlok-shah@users.noreply.github.com>
2023-06-01 23:44:45 +0530

    Update javascr.js to add tasks functionality

--- javascr.js
+++ javascr.js
@@ -1,8 +1,17 @@
 
+const Add = () => {
+    let x = document.getElementsByClassName("addtask")[0].value;
+    let y = document.getElementsByClassName("tasks")[0];
+    console.log(x);
+    console.log(y);
+    y.innerHTML += `<li class="todo">${x}</li><hr>`;
+
+}
 
 
 
-let api_key= "chqctl9r01qv883frg0gchqctl9r01qv883frg10"
+
+let api_key= "chqctl9r01qv883frg0gchqctl9r01qv883frg10";
 
 //https://finnhub.io/api/v1/quote?symbol=AAPL&token=chqctl9r01qv883frg0gchqctl9r01qv883frg10
 
